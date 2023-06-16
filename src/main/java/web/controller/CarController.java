f4308ce8c70028f830b9b0d59751ce5dc77cda76
Artemius987654321] <artyom.lazarev.1999@bk.ru]>
2023-06-16 08:07:17 +0700

    PP2.2.2
    PP2.2.2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 
 @Controller
 public class CarController {
-    private CarServiceImpl carService;
+    private CarService carService;
     @Autowired
     public CarController(CarServiceImpl carService) {
         this.carService = carService;
